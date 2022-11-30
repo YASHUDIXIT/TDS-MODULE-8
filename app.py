d27f05e6b67ad98d47349a4d057c55e713f26d3b
YASHUDIXIT <119510095+YASHUDIXIT@users.noreply.github.com>
2022-11-30 19:29:37 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.title("Odd Even ")
+num = st.number_input("Enter a number !",min_value = 1,step = 1)
+if (num % 2 == 0):
+         st.write("The number is even ")
+else:
+         st.write("The number is odd ")
